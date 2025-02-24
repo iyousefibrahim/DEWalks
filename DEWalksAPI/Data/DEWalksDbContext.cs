b231fcb8cb2dde0372b25fe1c9843b24f63252fd
Yousef <ioyx515@gmail.com>
2025-02-24 16:57:20 +0200

    Refactor project structure & integrate Entity Framework Core
    - Updated ".gitignore" to exclude sensitive "appsettings" files.
    - Removed unused "WeatherForecastController.cs" and "WeatherForecast.cs".
    - Added Entity Framework Core dependencies to "DEWalksAPI.csproj".
    - Configured "DbContext" in "Program.cs" with a connection string.
    - Created "DEWalksDbContext" for database management.
    - Introduced domain models: "Difficulty", "Region", and "Walk".

---
+++ DEWalksAPI/Data/DEWalksDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace DEWalksAPI.Data
+{
+    public class DEWalksDbContext : DbContext
+    {
+        public DEWalksDbContext(DbContextOptions contextOptions):base(contextOptions)
+        {
+            
+        }
+    }
+}
